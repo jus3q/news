280aba80130c3988880b790627103bce4abf29b7
jus3q <77412576+jus3q@users.noreply.github.com>
2022-06-06 15:55:27 -0700

    added comments to blog posts

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment #
 
+class CommentInline(admin.TabularInline): # new
+    model = Comment
+    # extra: 0
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin): # new
+    inlines = [
+    CommentInline,
+]
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
